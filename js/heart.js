b8773da2e9d8913095a208ad4191594e7a9cad1c
Aman-Amadeusz <52504242+Aman-Amadeusz@users.noreply.github.com>
2022-08-01 23:42:50 +0200

    heart beat

--- js/heart.js
+++ js/heart.js
@@ -1,25 +1,26 @@
 function heartBeat(){
     let heart = new Audio ("music/heart-beat.wav");
     let radius = document.querySelector(".frontWindowSec");
+
+console.log( radius.style, ' radius.style' );
+    
+    radius.style.position = "absolute";
+    radius.style.zIndex = "100";
+    radius.style.backgroundColor = "#black";
     
     let i = 0;
     let impuls = setInterval(() => {
         i++;
-
         radius.style.width = (radius.offsetWidth-10.50833892822266)+"px";
         radius.style.height = (radius.offsetHeight-10.50833892822266)+"px";
- 
-
         if(i == 16) {
             radius.style.width = (radius.offsetWidth+129)+"px";
             radius.style.height = (radius.offsetHeight+129)+"px";
             heart.play();
-
         }
         if(i == 19) {
             radius.style.width = (radius.offsetWidth+18.8)+"px";
             radius.style.height = (radius.offsetHeight+18.8)+"px";
-
             if(i == 19){
                 i = 0;
             }
