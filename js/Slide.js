412494fbe7e9b7289c0d7ddd7e2a96fa79921567
Aman-Amadeusz <52504242+Aman-Amadeusz@users.noreply.github.com>
2022-09-26 19:06:03 +0200

    Main lobby

---
+++ js/Slide.js
@@ -0,0 +1,7 @@
+var splide = new Splide( '.mainNav', {
+    type   : 'loop',
+    perPage: 3,
+    focus  : 'center',
+  } );
+  
+  splide.mount();
--- js/heart.js
+++ js/heart.js
@@ -1,18 +1,38 @@
 function heartBeat(){
     let heart = new Audio ("music/heart-beat.wav");
     let radius = document.querySelector(".frontWindowSec");
+    let secScene = document.querySelector(".secScene");
+    let frontWindow = document.querySelector(".frontWindow");
+//console.log(secScene, 'secScene');
 
-console.log( radius.style, ' radius.style' );
     
+    radius.style.width = '20%'; // 290
+    radius.style.height = '40%'; // 290
     radius.style.position = "absolute";
     radius.style.zIndex = "100";
     radius.style.backgroundColor = "#black";
-    
+
+    console.log(radius.offsetWidth, 'radius.offsetWidth2');
+    console.log(radius.offsetHeight, 'radius.offsetheigth2');
+    let diameter = (290 - 10);
+console.log(diameter, 'diameter');    
+
+
+
+
     let i = 0;
-    let impuls = setInterval(() => {
+   /* let impuls = setInterval(() => {
         i++;
+        let secSceneFullWidth = secScene.offsetWidth;
+        let secSceneFullHeight = secScene.offsetHeight;
+
+        frontWindow.style.width = '45%';
+        frontWindow.style.height = '20%';
+
+        
         radius.style.width = (radius.offsetWidth-10.50833892822266)+"px";
         radius.style.height = (radius.offsetHeight-10.50833892822266)+"px";
+
         if(i == 16) {
             radius.style.width = (radius.offsetWidth+129)+"px";
             radius.style.height = (radius.offsetHeight+129)+"px";
@@ -25,5 +45,5 @@ console.log( radius.style, ' radius.style' );
                 i = 0;
             }
         }
-    }, 135);
+    }, 135);*/
 }
