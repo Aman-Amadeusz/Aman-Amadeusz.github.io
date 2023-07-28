c29408380aba2832d0f4cdbcb24ff15bb6456ad7
Aman-Amadeusz <Nowacki.Amadeusz@gmail.com>
2023-07-28 23:04:11 +0200

    Update script_first.js

--- js/script_first.js
+++ js/script_first.js
@@ -1,6 +1,6 @@
 function firstInput(){
     let valueX = document.querySelector(".firstInput");
-    if(valueX.value == "a"){
+    if(valueX.value == ""){
         let up = document.querySelector(".up");
         let down = document.querySelector(".down");
         let i = 0;
